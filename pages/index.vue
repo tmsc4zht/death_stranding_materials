ded093363f9f15ea5124d5d131473bee598a13e7
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-04-11 21:21:25 +0900

    interface

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="container"></div>
+  <div class="container mx-auto">
+    <h1>Death Stranding Materilas</h1>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Material</th>
+            <th>Require</th>
+            <th>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Resins</td>
+            <td><input type="number" /></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Metals</td>
+            <td><input type="number" /></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Ceramics</td>
+            <td><input type="number" /></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Chemicals</td>
+            <td><input type="number" /></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Special alloys</td>
+            <td><input type="number" /></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
